46953fe5bfa8991f57e23630ea9b188c6d195f61
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2024-12-29 15:08:37 +0530

    Update LimitedEdition.jsx

--- Frontend/src/pages/LimitedEdition.jsx
+++ Frontend/src/pages/LimitedEdition.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import Header from '../components/Header'
-import HeroSection from '../components/HeroSection'
-import CtaBannerSection from '../components/CtaBannerSection'
-import SectionByLabel from '../components/SectionByLabel'
+import React from "react";
+import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import CtaBannerSection from "../components/CtaBannerSection";
+import SectionByLabel from "../components/SectionByLabel";
 
 const LimitedEdition = () => {
   return (
     <div>
-        <HeroSection video={'/assets/LimitedEditionSecVid.mp4'}/>
-        <CtaBannerSection heading= {'Sale Live Soon..'} subHeading={'We will be there with new designs soon...'} btnText={'Wait Is Fruitful'} image={'/assets/SaleCTA.png'}/>
-        <SectionByLabel/>
-        <SectionByLabel/>
+      <HeroSection video={"/assets/LimitedEditionSecVid.mp4"} />
+      <CtaBannerSection
+        heading={"Sale Live Soon.."}
+        subHeading={"We will be there with new designs soon..."}
+        btnText={"Wait Is Fruitful"}
+        image={"/assets/SaleCTA.png"}
+      />
+      <SectionByLabel />
+      <SectionByLabel />
     </div>
-  )
-}
+  );
+};
 
-export default LimitedEdition
+export default LimitedEdition;
